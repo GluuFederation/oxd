ad5c2359bcce2d7c0b604815ab2c41b34f83a9b7
Arnab Dutta <arnab.bdutta@gmail.com>
2020-11-03 18:22:03 +0530

    Revert swagger changes in #553.

--- oxd-gen-client/src/main/java/io/swagger/client/model/GetDiscoveryResponse.java
+++ oxd-gen-client/src/main/java/io/swagger/client/model/GetDiscoveryResponse.java
@@ -1152,7 +1152,7 @@ public class GetDiscoveryResponse {
 
 
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(java.lang.Object o) {
     if (this == o) {
       return true;
     }
@@ -1271,7 +1271,7 @@ public class GetDiscoveryResponse {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(Object o) {
+  private String toIndentedString(java.lang.Object o) {
     if (o == null) {
       return "null";
     }
--- oxd-gen-client/src/main/java/io/swagger/client/model/UmaRpGetRptParams.java
+++ oxd-gen-client/src/main/java/io/swagger/client/model/UmaRpGetRptParams.java
@@ -238,7 +238,7 @@ public class UmaRpGetRptParams {
 
 
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(java.lang.Object o) {
     if (this == o) {
       return true;
     }
@@ -285,7 +285,7 @@ public class UmaRpGetRptParams {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(Object o) {
+  private String toIndentedString(java.lang.Object o) {
     if (o == null) {
       return "null";
     }
