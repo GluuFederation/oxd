769c1f8ea242138ecb499ee2d7a42efafc6d1505
Yuriy Movchan <Yuriy.Movchan@gmail.com>
2023-12-05 15:24:44 +0300

    Version 4.5.3.Final

--- oxd-server/src/main/bin/oxd-download.sh
+++ oxd-server/src/main/bin/oxd-download.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-warName=oxd-server-4.5.3-SNAPSHOT-distribution.zip
+warName=oxd-server-4.5.3.Final-distribution.zip
 distDir=oxd-dist
 
 # Clean up
@@ -8,5 +8,5 @@ rm -f $warName
 rm -f -r $destDir
 
 # Download and unzip
-wget http://ox.gluu.org/maven/org/xdi/oxd-server/4.5.3-SNAPSHOT/$warName
+wget http://ox.gluu.org/maven/org/xdi/oxd-server/4.5.3.Final/$warName
 unzip $warName -d $distDir
